003172d321cfa0a989f2ebee3a88bbae54e7e42b
punnayansaha07 <punnayansaha18@gmail.com>
2024-12-25 13:43:06 +0530

    package.json and tailwind.config file corrected

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
 export default {
   content: [
     "./index.html",
@@ -7,7 +9,6 @@ export default {
   theme: {
     extend: {},
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require('daisyui'),],
+  plugins: [daisyui],
 }
 
